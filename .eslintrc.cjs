1f513a2a6670b0381c8480fa8d184a6d88c49c88
Cesar Novoa <novoafernandezcesar@gmail.com>
2025-06-25 22:00:01 +0100

    chore: update package.json to add linting and formatting scripts, and include new devDependencies
    feat: add Prettier configuration file for consistent code formatting
    
    fix: update tsconfig.json to include ESLint configuration file in the project structure

---
+++ .eslintrc.cjs
@@ -0,0 +1,37 @@
+// .eslintrc.cjs
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  parser: "astro-eslint-parser",
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+    extraFileExtensions: [".astro"],
+    sourceType: "module",
+    ecmaVersion: 2021,
+  },
+  plugins: ["@typescript-eslint", "astro", "prettier"],
+  extends: [
+    "eslint:recommended",
+    "plugin:astro/recommended", // reglas Astro
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended", // integra Prettier
+    "prettier", // desactiva reglas que choquen
+  ],
+  overrides: [
+    {
+      files: ["*.astro"],
+      rules: {
+        // aquí puedes desactivar o ajustar reglas solo para .astro
+      },
+    },
+  ],
+  rules: {
+    // fuerza estilo Prettier como error
+    "prettier/prettier": ["error"],
+    // tus ajustes extra…
+  },
+};
---
+++ prettier.config.cjs
@@ -0,0 +1,9 @@
+// prettier.config.cjs
+module.exports = {
+  printWidth: 100,
+  singleQuote: true,
+  trailingComma: "all",
+  proseWrap: "always",
+  plugins: [require("prettier-plugin-astro")],
+  pluginSearchDirs: false,
+};
