860ffb95cfbf8c09c35129084c123c40048babef
Cesar Novoa <novoafernandezcesar@gmail.com>
2025-06-26 12:12:10 +0100

    feat: add COLORS constant and ColorsType type definition

---
+++ src/constants/Colors.ts
@@ -0,0 +1,7 @@
+import type { ColorsType } from '../types/Colors';
+
+export const COLORS: Record<string, ColorsType> = {
+  WHITE: 'white',
+  BLACK: 'black',
+  MAIN: 'main',
+};
---
+++ src/types/Colors.ts
@@ -0,0 +1,1 @@
+export type ColorsType = 'white' | 'black' | 'main';
