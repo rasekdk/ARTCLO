6953304e49fdd08852d5144eabcecefde5c75ef5
Cesar Novoa <novoafernandezcesar@gmail.com>
2025-06-26 11:14:13 +0100

    chore: migrate Prettier configuration to .prettierrc.json and add prettier-plugin-tailwindcss

--- prettier.config.cjs
+++
@@ -1,9 +0,0 @@
-// prettier.config.cjs
-module.exports = {
-  printWidth: 100,
-  singleQuote: true,
-  trailingComma: 'all',
-  proseWrap: 'always',
-  plugins: [require('prettier-plugin-astro')],
-  pluginSearchDirs: false,
-};
