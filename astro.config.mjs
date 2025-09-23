d87dc09a5f2889d77d42f58e90fb0768b12d8fd3
César Nóvoa <themotokar@gmail.com>
2025-09-24 00:38:32 +0100

    chore: update astro version to 5.13.10 and remove preact dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
-import preact from '@astrojs/preact';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()],
   prefetch: true,
   vite: {
     plugins: [tailwindcss()],
