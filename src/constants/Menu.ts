6c0759e50d979a36646724c37961bd4703fd7857
César Nóvoa <themotokar@gmail.com>
2025-09-24 00:32:39 +0100

    feat: add disabled state to links and update menu items for better user experience

--- src/constants/Menu.ts
+++ src/constants/Menu.ts
@@ -5,21 +5,25 @@ export const MENU_ITEMS = [
     id: 'collection',
     label: 'Collection',
     url: ROUTES.COLLECTION,
+    disabled: false,
   },
   {
     id: 'clothing',
     label: 'Clothing',
     url: ROUTES.CLOTHING,
+    disabled: true,
   },
-  // {
-  //   id: 'footwear',
-  //   label: 'Footwear',
-  //   url: ROUTES.FOOTWEAR,
-  // },
+  {
+    id: 'footwear',
+    label: 'Footwear',
+    url: ROUTES.FOOTWEAR,
+    disabled: true,
+  },
   {
     id: 'culture',
     label: 'Culture',
     url: ROUTES.CULTURE,
+    disabled: false,
   },
 ] as const;
 
--- src/types/Components.ts
+++ src/types/Components.ts
@@ -19,6 +19,7 @@ export type LinkPropsType = HTMLAttributes<'a'> & {
   color?: ColorsType;
   isActive?: boolean;
   onClick?: (event: MouseEvent) => void;
+  disabled?: boolean;
 };
 
 export type CartScrollType = {
